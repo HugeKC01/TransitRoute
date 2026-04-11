ca2c93ef4de74a0831b96c6e1d996dae13c2c25e
HugeKC <20868603+HugeKC01@users.noreply.github.com>
2026-04-11 13:09:53 +0700

    Adjust weights in Dijkstra's algorithm for improved route optimization

--- lib/services/direction_service.dart
+++ lib/services/direction_service.dart
@@ -1630,9 +1630,10 @@ class DirectionService {
       _dijkstraWeightedPath(
         start,
       dest,
-      distanceWeight: 1.0,
-      timeWeight: 0.2, // fast rail
+      distanceWeight: 0.8,
+      timeWeight: 1.0,
       busCostPenalty: 5000.0,
+      transferPenalty: 2500.0,
       ).then((r) => addRoute(r, {'Rail priority'}))
     ]);
 
